940b48dd370f6aa10c7b212a1c66c65a5eb26419
Mario-gogo <oiram.ceballos@gmail.com>
2023-01-23 13:35:37 -0800

    tesing

---
+++ CEBALLOS_MARIO_JSPredict.js
@@ -0,0 +1,3 @@
+//Predict 1: I was born in 1980
+//Predict 2: The console.log would return "I was born in 1980" since birthYearInput is set to 1980
+//Predict 3: Summing Numbers!, num1 is 10, num2 is 20, 30
---
+++ Ceballos_Mario_JS_Tinkering.js
@@ -0,0 +1,9 @@
+var childHeight = 18;
+function displayIfChildIsAbleToRideTheRollerCoaster();
+if (childHeight>52);{
+
+console.log("Get on that ride, Kiddo!");
+}
+else {
+console.log("Sorry, Kiddo. Maybe next year.");
+}
